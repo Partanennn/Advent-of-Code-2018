9b780537db65a7b4db47f0db2fd6f0144fd2f586
Partanennn <44446649+Partanennn@users.noreply.github.com>
2018-12-05 14:23:15 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace C__testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
