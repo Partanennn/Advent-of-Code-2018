5ea6c416024d86bcdc68f416fa118a9d2f55742a
Partanennn <aleksi.partanenn17@gmail.com>
2018-12-07 23:05:36 +0200

    Day3Part1 save

--- Program.cs
+++ Program.cs
@@ -6,8 +6,32 @@ namespace C__testing
     {
         static void Main(string[] args)
         {
-            
-            Console.WriteLine("Hello World!");
+            String info = "#1 @ 1,3: 4x4";
+            int x = 10;
+            int y = 10;
+            char id;
+            int coordinate_x, coordinate_y, width, height;
+            String[,] fabric = new String[x, y];
+
+            for(int i = 0; i < x; i++) {
+                for(int j = 0; j < y; j++) {
+                    fabric[i, j] = ".";
+                    //Console.WriteLine(fabric[i, j]);
+                }
+            }
+            id = ' ';
+            foreach(char c in info) {
+                if(c == '#') {
+                    id = c;
+                } else if(id != )
+            }
+
+            for(int i = 0; i < x; i++) {
+                for(int j = 0; j < y; j++) {
+                    Console.Write(fabric[i, j]);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
